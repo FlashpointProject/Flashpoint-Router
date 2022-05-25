752e49ae4b584a470471d279c8d5d46fbf67d11a
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2022-05-24 19:12:25 -0600

    add Access-Control-Allow-Methods header

--- router.php
+++ router.php
@@ -316,8 +316,9 @@ function router_send_file_headers($file_headers) {
 	// this was moved here
 	// it's specifically here so it'll always be sent with any status code
 	if (ROUTER_ALLOW_CROSSDOMAIN === true) {
-		header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Headers: *');
+		header('Access-Control-Allow-Methods: *');
+		header('Access-Control-Allow-Origin: *');
 	}
 }
 
